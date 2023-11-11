b2325012b0ac9583bbbab9b37a747625476b0909
Cynthia <109437062+CynthiaUmuhire@users.noreply.github.com>
2023-11-11 11:53:18 +0200

    feat: the routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,44 @@
 import {RouterProvider, createBrowserRouter } from "react-router-dom"
-import Home, {loader as userLoader } from "./pages/Home"
+import Home, {loader as usersLoader } from "./pages/Home"
 import Profile from "./pages/Profile"
 import MainLayout from "./components/MainLayout"
-import Story from "./pages/Story"
+import Story, {loader as userLoader} from "./pages/Story"
+import Landing from "./pages/Landing"
+import Login from "./pages/Login"
+import AuthLayout from "./components/AuthLayout"
+import Signup from "./pages/signup"
 
 
 export default function App() {
   const routes = createBrowserRouter([
     {
-      path:"/",
+      path: "/",
+      element: <AuthLayout/>,
+      children: [{
+        index: true,
+          element: <Landing/>
+        },
+        {
+          path: "login",
+          element: <Login />
+        },{
+          path: "signup",
+          element: <Signup/>
+        }]
+      },
+    {
+      path:"/home",
       element: <MainLayout/>,
       children: [
         {
           index : true,
           element: <Home/>,
-          loader: userLoader
+          loader: usersLoader
         },
         {
-          path: "/1",
-          element: <Story/>
+          path: "/home/:id",
+          element: <Story/>,
+          loader: userLoader
         }
       ]
     },
