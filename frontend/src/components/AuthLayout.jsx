d86b538b38a530df38924260b4573d40452c58c8
Cynthia <109437062+CynthiaUmuhire@users.noreply.github.com>
2023-11-11 11:49:02 +0200

    feat: authenticated layout

---
+++ frontend/src/components/AuthLayout.jsx
@@ -0,0 +1,21 @@
+import { Outlet } from "react-router-dom";
+function AuthLayout() {
+  return (
+      <section className="h-screen bg-[url('/landing-background.jpg')]  bg-cover bg-no-repeat overflow-hidden">
+
+        <div className="h-16">
+          <nav className="w-full bg-transparent text-gray-300 py-2 px-4 ">
+            <div className="flex justify-between max-w-screen-2xl mx-auto">
+              <h1 className="font-bold text-center text-lg sm:text-xl underline hover:translate-x-4 pt-1 ml-8">Nurture Nest</h1>
+            </div>
+          </nav>
+        </div>
+        <section className="my-10">
+          <Outlet />
+        </section>
+      </section>
+
+  );
+}
+
+export default AuthLayout;
